a6f4b5adeead98267e70c7aabec93b82b06c5ba8
Richard Ashley <rashley@gmail.com>
2023-08-23 14:58:48 +0100

    moving archive

--- dataArchive.py
+++ dataArchive.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import config    # These are libraries that are part of meteopi
-import subprocess,argparse, json, sys, os, datetime
+import subprocess,argparse, json, sys, os, datetime, shutil
 import ephem
 
 def debugOut(message):
@@ -11,7 +11,8 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Script to copy files from the local machine (pi) to a network mounted archive.')
 	parser.add_argument('--debug', action="store_true", default=False, help='Add debug information to the output.' )
 	parser.add_argument('-c', '--config', type=str, default='meteopi.cfg', help='Configuration file.' )
-	
+	parser.add_argument('-m', '--makeMovie', action="store_true", default=False, help='Make the movie with FFMPEG.' )
+		
 	args = parser.parse_args()
 
 	debug = args.debug
@@ -25,11 +26,35 @@ if __name__ == "__main__":
 		sys.exit()
 		
 	print("Archive configuration %s"%json.dumps(config.archive))
+	localPath = config.archive['local']
 	
 	now = datetime.datetime.now()
 	nowSeconds = datetime.datetime.timestamp(now)
 	timeString = now.strftime("%Y%m%d_%H%M")
 	dateString = now.strftime("%Y%m%d")
+
+	print("Today is:", dateString)
+	yesterdayTemp = now - datetime.timedelta(days = 1)
+	yesterday = yesterdayTemp.replace(hour = 12)
+	print("Yesterday was:", yesterday)
+	yesterdayString = yesterday.strftime("%Y%m%d")
+	print(yesterdayString)
+	
+	rsyncCommand = ["rsync", "-avP"]
+	rsyncCommand.append(config.archive['rsync'] + "/%s*"%yesterdayString)
+	rsyncCommand.append(localPath+"/.")
+	print("Running:", rsyncCommand)
+	from subprocess import Popen, PIPE
+	#output, errors = Popen(archiveFolder, stdout=PIPE, stderr=PIPE).communicate()
+	subprocess.call(rsyncCommand)
+
+	rsyncCommand = ["rsync", "-avP"]
+	rsyncCommand.append(config.archive['rsync'] + "/%s*"%dateString)
+	rsyncCommand.append(localPath+"/.")
+	print("Running:", rsyncCommand)
+	from subprocess import Popen, PIPE
+	#output, errors = Popen(archiveFolder, stdout=PIPE, stderr=PIPE).communicate()
+	subprocess.call(rsyncCommand)
 
 	
 	# Get a list of all files in the local folder
@@ -44,9 +69,6 @@ if __name__ == "__main__":
 			fileInfo = { 'filename' : f, 'age' : ageDays}
 			fileData.append(fileInfo)
 	print(fileData)
-	print("Today is:", dateString)
-	yesterday = now - datetime.timedelta(days = 1)
-	print("Yesterday was:", yesterday)
 	
 	for location in config.locations:
 		if location['name'] == config.currentLocation:
@@ -83,23 +105,41 @@ if __name__ == "__main__":
 			nightExposures.append(f['filename'])
 	nightExposures.sort()
 	
-	listFile = open("nightexposures.list", "wt")
+
+	listFile = open("%s.list"%yesterdayString, "wt")
 	for n in nightExposures:
 		listFile.write(os.path.join(localPath, n) + "\n")
 	listFile.close()
 	
-	print("Written %d exposures to 'nightexposures.list'"%len(nightExposures))
+	print("Written %d filenames (exposures) to '%s.list'"%(len(nightExposures), yesterdayString))
 	
-	
-	user = os.getlogin()
-	ffmpegCommand = ["nice", "/home/%s/bin/pipeFFMPEG.bash"%user]
-	ffmpegCommand.append("nightexposures")
-	print("Running:", ffmpegCommand)
-	from subprocess import Popen, PIPE
-	#output, errors = Popen(archiveFolder, stdout=PIPE, stderr=PIPE).communicate()
-	subprocess.call(ffmpegCommand)
+	destinationPath = config.archive['location']
+		
+	if args.makeMovie:
+		user = os.getlogin()
+		ffmpegCommand = ["nice", "/home/%s/bin/pipeFFMPEG.bash"%user]
+		ffmpegCommand.append(yesterdayString)
+		print("Running:", ffmpegCommand)
+		from subprocess import Popen, PIPE
+		subprocess.call(ffmpegCommand)
 
+	
+		print("Copying movie %s, %s"%(yesterdayString + ".mp4", os.path.join(destinationPath, yesterdayString + ".mp4")))
+		shutil.copyfile(yesterdayString + ".mp4", os.path.join(destinationPath, yesterdayString + ".mp4"))
+	
+		# Move the movie from the home folder. 
+		print("Moving local copy of movie %s, %s"%(yesterdayString + ".mp4", os.path.join(localPath, yesterdayString + ".mp4")))
+		shutil.move(yesterdayString + ".mp4", os.path.join(localPath, yesterdayString + ".mp4"))
+		shutil.move(yesterdayString + ".list", os.path.join(localPath, yesterdayString + ".list"))
+
+	
 		#
 		# 
 		#
-	
+	# Now copy the sunset to sunrise original files to the archive folder.
+	destinationNewFolder = os.path.join(destinationPath, yesterdayString)
+	if not os.path.exists(destinationNewFolder):
+		os.mkdir(destinationNewFolder)
+	for f in nightExposures:
+		print("Copying file: %s to %s"%(os.path.join(localPath, f), os.path.join(destinationNewFolder, f)))
+		shutil.copyfile(os.path.join(localPath, f), os.path.join(destinationNewFolder, f))
